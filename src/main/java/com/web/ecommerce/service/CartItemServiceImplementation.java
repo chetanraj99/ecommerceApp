6309d08bfa9b0d0fc7203bbb752823c074c008a5
chetanraj99 <chetanrajpatel99@gmail.com>
2024-09-03 21:04:51 +0530

    commit

--- src/main/java/com/web/ecommerce/service/CartItemServiceImplementation.java
+++ src/main/java/com/web/ecommerce/service/CartItemServiceImplementation.java
@@ -37,7 +37,7 @@ public class CartItemServiceImplementation implements CartItemService {
 		CartItem item= findCartItemById(id);
 		User user=userService.findUserById(item.getUserId());
 		if(user.getId().equals(userId)) {
-			item.setQuantity(item.getQuantity());
+			item.setQuantity(cartItem.getQuantity());
 			item.setPrice(item.getQuantity()*item.getProduct().getPrice());
 			item.setDiscountedPrice(item.getProduct().getDiscountedPrice()*item.getQuantity());
 		}
